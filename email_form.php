11264d32fa2c6adcd14dcaea3cb96e54fa8ebf0d
Lionelharry <lionelharri@gmail.com>
2019-02-03 17:26:17 +0100

    mail form

--- email_form.php
+++ email_form.php
@@ -1,5 +1,5 @@
 <?php
-if(isset($_POST['email'])) {
+if(isset($_POST['submit'])) {
  
     $email_to = "lionelharri@gmail.com";
     $email_subject = "Candidature Inscription Canada";
@@ -12,16 +12,11 @@ if(isset($_POST['email'])) {
 	$email = $_POST['email']; 
     $message = $_POST['message']; 
  
- 
-    $email_message = "Form details below.\n\n";
- 
      
     function clean_string($string) {
       $bad = array("content-type","bcc:","to:","cc:","href");
       return str_replace($bad,"",$string);
     }
- 
-     
  
     $email_message .= "Prénom: ".clean_string($prenom)."\n";
     $email_message .= "Nom: ".clean_string($nom)."\n";
@@ -29,9 +24,9 @@ if(isset($_POST['email'])) {
     $email_message .= "Telephone: ".clean_string($tel)."\n";
     $email_message .= "Message: ".clean_string($message)."\n";
  
-// create email headers
-$headers = 'De: '.$email."\r\n".
-'Répondre à: '.$email."\r\n" .
-'X-Mailer: PHP/' . phpversion();
-mail($email_to, $email_subject, $email_message, $headers);  
+	// create email headers
+	$headers = "De: ".$email."\n\n";
+	mail($email_to, $email_subject, $email_message, $headers);
+	header("Location: index.html");
+}	
 ?>
