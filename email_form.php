e2f2f21700fb99ee58457c3ad183fc81d57653c5
Lionelharry <lionelharri@gmail.com>
2019-02-05 19:29:30 +0100

    modal display

--- email_form.php
+++ email_form.php
@@ -1,34 +1,35 @@
 <?php
 
-print_r($_POST);
+
 if(isset($_POST['submit'])) {
- 
+    $display_modal = true;
+    
     $email_to = "info@inscriptioncanada.com";
     $email_subject = "Candidature Inscription Canada";
-     
- 
+
+
     $prenom = $_POST['prenom'];
-    $nom = $_POST['nom']; 
-    $age = $_POST['age']; 
-	$tel = $_POST['tel']; 
-	$email = $_POST['email']; 
-    $message = $_POST['message']; 
- 
-     
+    $nom = $_POST['nom'];
+    $age = $_POST['age'];
+	$tel = $_POST['tel'];
+	$email = $_POST['email'];
+    $message = $_POST['message'];
+
+
     function clean_string($string) {
       $bad = array("content-type","bcc:","to:","cc:","href");
       return str_replace($bad,"",$string);
     }
- 
+
     $email_message .= "Prénom: ".clean_string($prenom)."\n";
     $email_message .= "Nom: ".clean_string($nom)."\n";
     $email_message .= "Age: ".clean_string($age)."\n";
     $email_message .= "Telephone: ".clean_string($tel)."\n";
     $email_message .= "Message: ".clean_string($message)."\n";
- 
+
 	// create email headers
 	$headers = "De: ".$email."\n\n";
 	mail($email_to, $email_subject, $email_message, $headers);
 	header("Location: https://test-ic.netlify.com/");
-}	
+}
 ?>
